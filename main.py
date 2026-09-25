feba292c2f46dd23c44173f188e53cbeba19ac7b
Anderson <andersontopaga2018@gmail.com>
2026-09-25 15:27:17 -0500

    corregir limpieza de productos agotados

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ class TiendaOnline:
         return total_pedido
 
     def limpiar_agotados(self):
-        """Elimina del inventario los productos con cantidad 0 o menor."""
-        for id_producto in self.inventario.keys():
-            if self.inventario[id_producto]['cantidad'] <= 0:
-                del self.inventario[id_producto]
+    """Elimina del inventario los productos con cantidad 0 o menor."""
+    for id_producto in list(self.inventario.keys()):
+        if self.inventario[id_producto]['cantidad'] <= 0:
+            del self.inventario[id_producto]
 
 
 # --- CÓDIGO DE PRUEBA (Para que los estudiantes ejecuten) ---
